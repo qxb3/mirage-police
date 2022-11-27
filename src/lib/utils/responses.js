fc2236efe2a8af01bfb2e7b3aab6f60f16fca05d
qxb3 <qxbthree@gmail.com>
2022-11-27 10:44:10 +0800

    ref(responses utils)

--- src/lib/utils/responses.js
+++ src/lib/utils/responses.js
@@ -20,7 +20,7 @@ class Embed {
 
   static createCommandError(commandName, errorMessage) {
     return this.createError(errorMessage)
-      .setFooter({ text: `Use command: "!help ${commandName}" to learn more` })
+      .setFooter({ text: `Use "!help ${commandName}" commamd to learn more` })
   }
 }
 
