508ad689e55ac9c2e8fa03d20308c3af3d7f569f
qxb3 <qxbthree@gmail.com>
2022-11-27 09:29:23 +0800

    feat: added vars.js

---
+++ src/vars.js
@@ -0,0 +1,12 @@
+const { Constants } = require('discord.js')
+
+const Colors = {
+  Primary: Constants.Colors.AQUA,
+  Secondary: Constants.Colors.DARK_BUT_NOT_BLACK,
+  Success: Constants.Colors.GREEN,
+  Error: Constants.Colors.RED
+}
+
+module.exports = {
+  Colors
+}
